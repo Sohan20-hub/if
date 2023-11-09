baec1b87efc7233ba2146e3095b9daeb73ab885e
sohan <102313089+Sohan20-hub@users.noreply.github.com>
2023-11-09 10:41:30 +0530

    Update file3.sh

--- file3.sh
+++ file3.sh
@@ -1,1 +1,12 @@
-
+#!\bin\bash
+echo "Enter the numbers"
+read n1 n2 n3
+if [ $n1 -gt $n2 ] && [ $n1 -gt $n3 ]
+then
+echo "$n1 is greater"
+elif [ $n2 -gt $n1 ] && [ $n2 -gt $n3 ]
+then
+echo "$n2 is greater"
+else
+echo "$n3 is greater"
+fi
